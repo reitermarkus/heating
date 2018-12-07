db3281e30698b33350d33fd9bd6ca6dd3b09c95e
Markus Reiter <me@reitermark.us>
2018-12-07 09:46:59 +0100

    Add native GPIO version.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use std::time::{Instant, Duration};
+use std::error::Error;
 use std::sync::mpsc::channel;
+use std::time::{Instant, Duration};
 use std::thread;
 
 extern crate medianheap;
 extern crate ordered_float;
+extern crate rppal;
+extern crate sysfs_gpio;
 
 use medianheap::MedianHeap;
 use ordered_float::NotNan;
+use rppal::gpio::{Gpio, Level::*, Mode::*, Trigger};
+use sysfs_gpio::{Direction, Pin, Edge::*};
 
 mod tank;
 use tank::Tank;
@@ -14,21 +19,61 @@ use tank::Tank;
 mod cuboid_tank;
 use cuboid_tank::CuboidTank;
 
-extern crate sysfs_gpio;
-use sysfs_gpio::{Direction, Pin, Edge::*};
-
 const TEMPERATURE: f64 = 15.5; // °C
 const SPEED_OF_SOUND: f64 = 331.5 + 0.6 * TEMPERATURE; // m/s
 
-fn measure() -> Option<f64> {
-  let trigger = Pin::new(17);
-  let echo = Pin::new(18);
+const TRIGGER_PIN: u8 = 17;
+const ECHO_PIN:    u8 = 18;
+
+fn measure_native(gpio: &mut Gpio) -> Option<f64> {
+  gpio.write(TRIGGER_PIN, High);
+  thread::sleep(Duration::from_micros(10));
+  gpio.write(TRIGGER_PIN, Low);
+
+  let mut start = None;
+  let mut stop = None;
+
+  loop {
+    match gpio.poll_interrupt(ECHO_PIN, false, Some(Duration::from_millis(200))) {
+      Ok(Some(High)) => {
+        if start.is_none() {
+          start = Some(Instant::now());
+        }
+      },
+      Ok(Some(Low)) => {
+        if start.is_some() && stop.is_none() {
+          stop = Some(Instant::now())
+        }
+      },
+      Ok(None) => break,
+      Err(err) => {
+        eprintln!("Error: {}", err);
+        return None
+      },
+    }
+  }
+
+  if start.is_none() {
+    eprintln!("No rising edge detected.");
+  }
+
+  if stop.is_none() {
+    eprintln!("No falling edge detected.");
+  }
+
+  start.and_then(|start| stop.map(|stop| echo_duration_to_m(stop - start)))
+}
+
+fn measure_sysfs() -> Option<f64> {
+  let trigger = Pin::new(TRIGGER_PIN as u64);
+  let echo = Pin::new(ECHO_PIN as u64);
 
   let (sender, receiver) = channel();
 
   trigger.with_exported(|| {
     echo.with_exported(|| {
       trigger.set_direction(Direction::Out)?;
+      trigger.set_value(0)?;
       echo.set_direction(Direction::In)?;
 
       let mut poller = echo.get_poller()?;
@@ -54,9 +99,7 @@ fn measure() -> Option<f64> {
           },
         };
 
-        let duration = stop - start;
-        let echo_length = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
-        let distance = echo_length / 2.0 * SPEED_OF_SOUND; // m
+        let distance = echo_duration_to_m(stop - start);
         sender.send(Some(distance)).unwrap();
 
         Ok(())
@@ -73,23 +116,61 @@ fn measure() -> Option<f64> {
   receiver.recv().unwrap()
 }
 
-fn main() {
-  let tank = CuboidTank::new(298.0, 148.0, 150.0);
+#[inline(always)]
+fn echo_duration_to_m(duration: Duration) -> f64 {
+  let echo_length = duration.as_secs() as f64 + duration.subsec_nanos() as f64 * 1e-9;
+  echo_length / 2.0 * SPEED_OF_SOUND // m
+}
 
-  println!("Volume: {}", tank.volume());
-  println!("Level: {}", tank.level());
+fn main() -> Result<(), Box<Error>> {
+  let mut native_heap = MedianHeap::new();
 
-  let mut heap = MedianHeap::new();
+  {
+    let mut gpio = Gpio::new()?;
 
-  for _ in 0..100 {
-    let measurement = measure();
+    gpio.set_mode(TRIGGER_PIN, Output);
+    gpio.write(TRIGGER_PIN, Low);
+    gpio.set_mode(ECHO_PIN, Input);
 
-    println!("Length: {:?}", measurement);
+    gpio.set_interrupt(ECHO_PIN, Trigger::Both)?;
 
-    if let Some(measurement) = measurement {
-      heap.push(NotNan::from((measurement * 1000.0).round() / 1000.0))
+    for _ in 0..10 {
+      if let Some(distance) = measure_native(&mut gpio) {
+        println!("Distance: {:?}", distance);
+
+        native_heap.push(NotNan::from((distance * 1000.0).round() / 10.0));
+      }
     }
   }
 
-  println!("Median: {:?}", heap.median());
+  println!();
+
+  let mut sysfs_heap = MedianHeap::new();
+
+  for _ in 0..10 {
+    if let Some(distance) = measure_sysfs() {
+      println!("Distance: {:?}", distance);
+
+      sysfs_heap.push(NotNan::from((distance * 1000.0).round() / 10.0))
+    }
+  }
+
+  println!();
+
+  println!("Native Median: {:?}", native_heap.median());
+  println!("SysFS Median: {:?}", sysfs_heap.median());
+
+  let mut tank = CuboidTank::new(298.0, 148.0, 150.0);
+
+  println!("Volume: {}", tank.volume());
+
+  let filled_height = tank.height - native_heap.median().unwrap().into_inner();
+
+  tank.set_filled_height(filled_height);
+
+  println!("Level: {}", tank.level());
+
+  println!("Filled Volume: {}", tank.level() * tank.volume());
+
+  Ok(())
 }
