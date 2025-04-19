54d41eceba32eea09ddfc8e166c4b2462d934758
Markus Reiter <me@reitermark.us>
2025-04-19 21:44:43 +0200

    Remove unused crates.

--- vessel/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod level;
-
-mod tank;
-pub use crate::tank::Tank;
-
-mod cuboid_tank;
-pub use crate::cuboid_tank::CuboidTank;
--- vessel/src/tank.rs
+++
@@ -1,8 +0,0 @@
-use measurements::{Length, Volume};
-
-use crate::level::Level;
-
-pub trait Tank {
-  fn volume(&self) -> Volume;
-  fn level(&self, filling_height: Length) -> Level;
-}
